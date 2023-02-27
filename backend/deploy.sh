efc097e85601268bbbdc465bd66c723e53b6e3a5
Pronin Konstantin <k.pronin@qiwi.com>
2023-02-27 22:06:18 +0500

    blue/green deployment

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -14,4 +14,12 @@ EOF
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 set -e
 
-docker-compose up -d backend
+if docker ps | grep -q backend-blue; then
+    docker compose stop backend-green
+    docker compose up --wait backend-green
+    docker compose stop backend-blue
+else    
+    docker compose stop backend-blue
+    docker compose up --wait backend-blue
+    docker compose stop backend-green
+fi
